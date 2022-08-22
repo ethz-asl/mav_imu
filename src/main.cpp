d805c685871773330dd2a2feaa070f3d51bf742f
4c3y <69460051+4c3y@users.noreply.github.com>
2022-08-22 16:44:10 +0200

    Bugfixes and refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -5,46 +5,46 @@
 #include <cstring>
 #include <iostream>
 
+
 int main() {
   std::cout << "Test" << std::endl;
-  char *name = "/dev/spidev0.1";
-  struct spi_ioc_transfer xfer[2];
-  unsigned char buf[32], *bp;
-  int status;
+  std::string name = "/dev/spidev0.1";
+  struct spi_ioc_transfer xfer[1];
+  unsigned char buf[32]{};
+  unsigned char *bp;
 
-  int fd = open(name, O_RDWR);
+  int fd = open(name.data(), O_RDWR);
   if (fd < 0) {
-    perror("open");
+    std::cout << "Error opening file: " << strerror(errno);
     return 1;
   }
 
   memset(xfer, 0, sizeof xfer);
   memset(buf, 0, sizeof buf);
 
-/*
-* Send a read command
-*/
-  buf[0] = 0x04;
+// Send a read command
+  buf[0] = 0x0E;
   buf[1] = 0x00;
 
   xfer[0].tx_buf = (unsigned long) buf;
   xfer[0].len = 2;
 
-  int len = 4;
+  int len = 2;
   unsigned char buf2[len];
-  xfer[1].rx_buf = (unsigned long) buf;
-  xfer[1].len = 4;
+  xfer[0].rx_buf = (unsigned long) buf2;
+  xfer[0].len = len;
 
-  status = ioctl(fd, SPI_IOC_MESSAGE(2), xfer);
+  int status = ioctl(fd, SPI_IOC_MESSAGE(2), xfer);
   if (status < 0) {
     perror("SPI_IOC_MESSAGE");
     return -1;
   }
 
   printf("response(%d): ", status);
-  for (bp = buf2; len; len--)
+  for (bp = buf2; len; len--) {
     printf("%02x ", *bp++);
-  printf("\n");
+  }
+  std::cout << std::endl;
 
   return 0;
 }
